90222a3e3a83cdb3a1a6e9c2a0e2099a8ed19591
prathamdighe <prathamdighe39@gmail.com>
2025-04-10 23:56:33 +0530

    my commit rcg

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Accordian from './components/accordion';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
-      <Accordian />
+      {/*<Accordian />*/}
+
+      <RandomColor/>
     </div>
   );
 }

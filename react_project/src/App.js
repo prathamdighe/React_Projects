ca87e45e3a6d64d614a16465f2950738e45edda7
prathamdighe <prathamdighe39@gmail.com>
2025-04-14 18:16:45 +0530

    star-mycommit

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,18 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import Accordian from './components/accordion';
-import RandomColor from './components/random-color';
+import logo from "./logo.svg";
+import "./App.css";
+import Accordian from "./components/accordion";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
       {/*<Accordian />*/}
-
-      <RandomColor/>
+      {/*<RandomColor/>*/}
+      <StarRating/>
     </div>
   );
 }
 
 export default App;
-
-
